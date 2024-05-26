a7c43b79fdb2b55ce98f2385c873a0c1f3c602ed
TiavinaMichael <ralainirinatiavina@gmail.com>
2024-05-26 16:41:07 +0000

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <h1>cool 2</h1>;
+  return <h1 className="text-3xl font-bold underline">cool 3</h1>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { StyledEngineProvider } from "@mui/material/styles";
 import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
+    <App />
   </React.StrictMode>,
 );
