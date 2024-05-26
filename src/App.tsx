07588a6219b152d88b0bf7891d900515be92cfd6
TiavinaMichael <ralainirinatiavina@gmail.com>
2024-05-26 16:33:24 +0000

    fix config

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <h1>cool 2</h1>;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { StyledEngineProvider } from "@mui/material/styles";
+import App from "./App";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      <h1>cool</h1>
+      <App />
     </StyledEngineProvider>
   </React.StrictMode>,
 );
