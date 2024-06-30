b37be2d0efe791d6ecfd1eec4b49f363bde83061
tiavina-mika <tiavinamika@gmail.com>
2024-06-30 06:19:43 +0300

    Create index.ts

---
+++ demo/src/index.ts
@@ -0,0 +1,12 @@
+import TiptapParser from './TiptapParser';
+
+export {
+  type TiptapProps,
+  type ClassNamesProps
+} from './TiptapParser';
+
+export {
+  type HTMLReactParserOptions
+} from 'html-react-parser';
+
+export default TiptapParser;
